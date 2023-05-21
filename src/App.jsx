426ee972c7dd9926f1c0688710254d8a5323bdbf
adenugbamichael <adenugbamicke@gmail.com>
2023-05-21 10:43:15 +0100

    Conditional rendering | event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion"
 function App() {
   const items = [
     {
+      id: "one",
       label: "Can I use React on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "two",
       label: "Can I use Javascript on a project?",
       content:
         "You can use Javascript on any project you want. You can use Javascript on any project you want.",
     },
     {
+      id: "three",
       label: "Can I use CSS on a project?",
       content:
         "You can use CSS on any project you want. You can use CSS on any project you want.",
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,21 @@
 /* eslint-disable react/prop-types */
-const Accordion = ({ items }) => {
-  return <div />
+import { useState } from "react"
+
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(0)
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex
+
+    return (
+      <div key={item.id}>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    )
+  })
+
+  return <div>{renderedItems}</div>
 }
 
 export default Accordion
