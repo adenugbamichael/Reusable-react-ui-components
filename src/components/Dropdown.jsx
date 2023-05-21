0d7305cb7aff8e05b73eb45b40fc3a9ce1f5f99c
adenugbamichael <adenugbamicke@gmail.com>
2023-05-21 20:19:34 +0100

    Reminder on event handlers in maps

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 const Dropdown = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(!isOpen)
+    // WHAT OPTION DID THE USR CLICK ON???
+    console.log(option)
+  }
+
+  const handleOptionClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    )
   })
 
   return (
     <div>
       {" "}
-      <div>Select...</div>
-      <div>{renderedOptions}</div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
     </div>
   )
 }
