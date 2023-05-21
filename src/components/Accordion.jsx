3ac387d9e7f2cd4060b0d9b9a394044d0f5a15ff
adenugbamichael <adenugbamicke@gmail.com>
2023-05-21 13:08:48 +0100

    Toggling panel collapse

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,11 +5,22 @@ import { GoChevronDown, GoChevronLeft } from "react-icons/go"
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0)
 
+  const handleClick = (nextIndex) => {
+    expandedIndex === nextIndex
+    setExpandedIndex(nextIndex)
+  }
+  //   if (expandedIndex === nextIndex) {
+  //     setExpandedIndex(-1)
+  //   } else {
+  //     setExpandedIndex(nextIndex)
+  //   }
+  // }
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
     const icon = (
-      <span className='text-xl'>
+      <span className='text-2xl'>
         {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
       </span>
     )
@@ -18,12 +29,11 @@ function Accordion({ items }) {
       <div key={item.id}>
         <div
           className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'
-          onClick={() => setExpandedIndex(index)}
+          onClick={() => handleClick(index)}
         >
           {item.label}
           {icon}
         </div>
-
         {isExpanded && <div className='border-b p-5'>{item.content}</div>}
       </div>
     )
