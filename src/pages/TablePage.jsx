a2c4941c9631413b452a17152667006350f0c267
adenugbamichael <adenugbamicke@gmail.com>
2023-05-24 06:07:44 +0100

    nested maps | fixing color

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -11,12 +11,15 @@ const TablePage = () => {
   const config = [
     {
       label: "Name",
+      render: (fruit) => fruit.name,
     },
     {
       label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
     {
       label: "Score",
+      render: (fruit) => fruit.score,
     },
   ]
   return (
