ffaf8ebe486a67fa40fbfc7a65332a5a4fbf898c
adenugbamichael <adenugbamicke@gmail.com>
2023-05-24 02:00:55 +0100

    refactor: dynamic headers

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable react/prop-types */
-const Table = ({ data }) => {
+const Table = ({ data, config }) => {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>
+  })
+
   const renderedRows = data.map((fruit) => {
     return (
       <tr className='border-b' key={fruit.name}>
@@ -14,11 +18,7 @@ const Table = ({ data }) => {
   return (
     <table className='table-auto border-spacing-2'>
       <thead>
-        <tr className='border-b-2'>
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className='border-b-2'>{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
