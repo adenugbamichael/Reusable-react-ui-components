2fba16d4ba98b4b41874f8ba5ae365ce6ddc5c3f
adenugbamichael <adenugbamicke@gmail.com>
2023-05-24 20:10:31 +0100

    Determining Icon set | Styling Header cells

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
+import { GoArrowSmallUp, GoArrowSmallDown } from "react-icons/go"
 import Table from "./Table"
 
 const SortableTable = (props) => {
@@ -27,8 +28,14 @@ const SortableTable = (props) => {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          {column.label} IS SORTABLE
+        <th
+          className='cursor-pointer hover:bg-gray-100'
+          onClick={() => handleClick(column.label)}
+        >
+          <div className='flex items-center'>
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
         </th>
       ),
     }
@@ -56,6 +63,38 @@ const SortableTable = (props) => {
       <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
   )
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    )
+  }
+
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    )
+  } else if (sortOrder === "asc") {
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    )
+  } else if (sortOrder === "desc") {
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    )
+  }
 }
 
 export default SortableTable
