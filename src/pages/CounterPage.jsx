96f1556710e118f95d72ab84f134b3319955f203
adenugbamichael <adenugbamicke@gmail.com>
2023-05-25 19:47:15 +0100

    Immer in Action

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { produce } from "immer"
 import { useReducer } from "react"
 import Button from "../components/Button"
 import Panel from "../components/Panel"
@@ -11,36 +12,28 @@ const ADD_VALUE_TO_COUNT = "add-value-to-add"
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      }
+      state.count = state.count + 1
+      return
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      }
+      state.count = state.count - 1
+      return
 
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      }
+      state.count = state.count + state.valueToAdd
+      state.valueToAdd = 0
+      return
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      }
+      state.valueToAdd = action.payload
+      return
     default:
-      return state
+      return
   }
 }
 
 function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount)
   // const [valueToAdd, setValueToAdd] = useState(0)
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   })
